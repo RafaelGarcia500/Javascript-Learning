8d9315d379a16f939a30fe6556f855bcef100d79
Rafa628 <91357930+Rafa628@users.noreply.github.com>
2022-04-29 11:14:33 -0300

    First part of a simple formulary

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+const year = 2022
+
+alert(`Hello, i want to ask to you a little bit of data about yourself`)
+
+let name = prompt(`Please, tell me your name.`)
+let surname = prompt(`Would you tell me your surname?`)
+let age = prompt(`Let me know your age`)
+
+let results = year - age 
+
+ 
+  
+     
+
