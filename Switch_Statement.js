04797bf0eb565224106fa36263ccc0d8ae00468b
Rafael <rafanavone@gmail.com>
2022-05-03 09:53:39 -0300

    Learned Switch conditional

---
+++ Switch_Statement.js
@@ -0,0 +1,28 @@
+const day= new Date().getDay()//Learned new tool!
+
+switch(day){
+    case 0:
+        alert(`Fill in Supermarket shelf`)
+        break
+
+    case 1:
+        alert(`Rest Day`)
+        break
+    case 2:
+        alert(`Work`) 
+        break
+    case 3:
+        alert(`Boring day`) 
+        break
+    case 4:
+        alert(`Bad Work Day`)
+        break
+    case 5:
+        alert(`Work`) 
+        break
+    case 6:
+        alert(`Work`) 
+        break
+
+    default: alert(`Something went wrong`)
+}
