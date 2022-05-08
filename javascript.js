e772b3af556b38c304f34854ae07d7d5b5f888df
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-08 10:17:41 -0300

    functions not yet clear

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
-let username=`john`
-
-function showMessage(){
-    username=`bob`
-    let message=`hola`
-    alert(message+` ` +username)
+function add7(a){
+ return(a+7)
 }
-alert(username)
 
-showMessage()
+let result=add7(900)
+alert(result)
+
+function multiply(a,b){
+    return(a*b)
+}
+
+ result=multiply(320,870)
+ alert(result)
+
+
+ function capitalize(string){
+       return string.charAt(0).toUpperCase()+string.slice(1)
+
+     
+ }
+
+ alert( capitalize(`string`) )
+
+ function lastLetter(text){
+     return text.charAt(text.lenght -1)
+ }
+
+ alert(lastLetter(`Becerra`))
+
