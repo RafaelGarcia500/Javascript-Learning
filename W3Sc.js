aee9c7d7633e1251301599b0342ba357574af480
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-20 22:22:42 -0300

    Delete W3Sc.js

--- W3Sc.js
+++
@@ -1,9 +0,0 @@
-let offical_name=`ECMAScript`
-
-let name= prompt(`What's the “official” name of JavaScript?`)
-
-if(name!==`ECMAScript`){
-    alert(`You don't know? “ECMAScript”!`)
-}else{
-    alert(`Right!`)
-}
