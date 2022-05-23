2314027e75bc0db3d150e51cbb5fe130d4b5e961
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-23 05:59:53 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
+function myCars()
 const cars = [`Saab`,`Volvo`,`BMW`]
 cars[0]=`Opel`
 
+function person()
 const person= {firstName:`John`, lastName:`Doe`,age:`43`}
+
+function fruitsLength()
+const fruits= [`Banana`,`Orange,`,`Apple`,`Mango`]
+let lenght = fruits.lenght
+
+
+myArray = []
+myArray[0]= myCars()
+myArray[1]=person()
+myArray[2]=fruitsLength()
