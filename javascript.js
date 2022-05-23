67a545982e80d12dc1dcef612ec17a68a319745b
RafaelGarcia500 <91357930+RafaelGarcia500@users.noreply.github.com>
2022-05-23 05:50:51 -0300

    arrays

--- javascript.js
+++ javascript.js
@@ -1,27 +1,3 @@
-let answer= parseInt(prompt(`Enter a number`))
-
-
-for(let i=1; i<=answer;i++){
-    switch(answer){
-        case (i%3 ==0 && i%5==0 ):
-            console.log(`FizzBuzz`)
-            break
-    
-        case (i%3==0):
-            console.log(`Fizz`)
-            break
-    
-        case (i%5==0):
-            console.log(`Buzz`)
-            break
-    
-        default:
-            console.log(i)
-    }
-}
-
-
-
-
-
+const cars = [`Saab`,`Volvo`,`BMW`]
+cars[0]=`Opel`
 
